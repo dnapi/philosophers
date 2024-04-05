ce7dc636078c228e9b4dc136d1b846d3b9d214b6
Alexandr Pimikov <apimikov@student.hive.fi>
2024-04-05 15:55:48 +0300

    test mode

--- philo/main.c
+++ philo/main.c
@@ -12,16 +12,19 @@ typedef struct s_philo
 }	t_philo;
 */
 
+int panic_args(char *msg, int return_value)
+{
+	printf("%s: %s\n", MSG_NAME, msg);
+	return (return_value);
+}
+
 int	check_args(int argc, char **argv)
 {
 	int		i;
 	char	*s;
 
 	if (argc < 5 || 6 < argc)
-	{
-		printf("phil: wrong number of arguments\n");
-		return (1);
-	}
+		return (panic_args(ERRMSG_NUMARG, 1));
 	i = 0;
 	while (++i < argc)
 	{
@@ -30,10 +33,7 @@ int	check_args(int argc, char **argv)
 		{
 //			printf("%c =%d, 0=%d, 9=%d\n", *s, (int)*s, (int)'0', (int)'9');
 			if (*s < '0' || '9' < *s)
-			{
-				printf("phil: arguments must be integers \n");
-				return (1);
-			}
+				return (panic_args(ERRMSG_INT, 1));
 			s++;
 		}
 	}
@@ -45,7 +45,35 @@ int	set_phil(t_philo *philo, int argc, char **argv)
 	if (check_args(argc, argv))
 		return (1);
 	philo->num = ft_atoil(argv[1]);
+	if (philo->num < 1)
+		return (panic_args(ERRMSG_NUMPHIL, 1));
+	philo->die = ft_atoil(argv[2]);
+	if (philo->die < 1)
+		return (panic_args(ERRMSG_TIMEDIE, 1));
+	philo->eat = ft_atoil(argv[3]);
+	if (philo->eat < 1)
+		return (panic_args(ERRMSG_TIMEEAT, 1));
+	philo->sleep = ft_atoil(argv[4]);
+	if (philo->sleep < 1)
+		return (panic_args(ERRMSG_TIMESLE, 1));
+	philo->max_eat = ft_atoil(argv[5]);
 	return (0);
+}
+
+void print_debug_mode(int flag, t_philo *philo)
+{
+	if (flag)
+	{
+
+		printf("-----------   DEBUG MODE  -------- \n");
+		printf("args are fine\n");
+		printf("num=%d\n", philo->num);
+		printf("die=%d\n", philo->die);
+		printf("eat=%d\n", philo->eat);
+		printf("sleep=%d\n", philo->sleep);
+		printf("max_eat=%d\n", philo->max_eat);
+		printf("-----------   END OF DEBUG  -------- \n");
+	}
 }
 
 int	main(int argc, char *argv[])
@@ -54,7 +82,6 @@ int	main(int argc, char *argv[])
 
 	if (set_phil(&philo, argc, argv))
 		return (1);
-	printf("args are fine\n");
-	printf("num of philo=%d\n", philo.num);
+	print_debug_mode(DEBUG_MOD, &philo);
 	return (0);
 }
--- philo/philo.h
+++ philo/philo.h
@@ -1,6 +1,16 @@
 #ifndef PHILO_H
 # include <stdio.h>
 
+# define MSG_NAME "phil"
+# define ERRMSG_NUMARG "wrong number of arguments"
+# define ERRMSG_INT "arguments must be integers"
+# define ERRMSG_NUMPHIL "number of philosofers must put positive"
+# define ERRMSG_TIMEDIE "time to die must put positive"
+# define ERRMSG_TIMEEAT "time to eat must put positive"
+# define ERRMSG_TIMESLE "time to sleep must put positive"
+
+# define DEBUG_MOD 1
+
 typedef struct s_philo
 {
     int num;
